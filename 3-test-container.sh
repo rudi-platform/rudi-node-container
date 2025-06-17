8b4dbb2c97a80c75212763db98333cd4b87d2866
Olivier Martineau <olivier.martineau@irisa.fr>
2025-06-17 14:32:09 +0200

    Update irisa branch with latest state of release branch

--- 3-test-container.sh
+++ 3-test-container.sh
@@ -12,7 +12,7 @@ TIME_START=$(now_ms_int)
 # Write your own configuration in 'container-conf.sh' file
 test -r ./container-conf.sh && source ./container-conf.sh
 
-VERSION="${VERSION:-"2.5.4"}"
+VERSION="${VERSION:-"2.5.5"}"
 
 # REGISTRY=ghcr.io/rudi-platform
 REGISTRY="${REGISTRY:-"registry.aqmo.org/public-rudi/public-packages"}"
