64634aa20ec379bf27e9683ac8d33193c5881338
Olivier Martineau <olivier.martineau@irisa.fr>
2025-06-17 14:53:19 +0200

    doc: connection to RUDI portal

--- 2-build-image.sh
+++ 2-build-image.sh
@@ -10,7 +10,7 @@ TIME_START=$(now_ms_int)
 test -r ./container-conf.sh && source ./container-conf.sh
 
 IMG_NAME="${IMG_NAME:-"rudinode"}"
-VERSION="${VERSION:-"2.5.4"}"
+VERSION="${VERSION:-"2.5.5"}"
 PLATFORMS=${PLATFORMS:-("linux/amd64" "linux/arm64")}
 
 # Build and tag for each platform
